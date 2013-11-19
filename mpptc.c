0df7de7bf175af1e0f3ccd6497076a656bd29586
Mike <tyrlikmike@gmail.com>
2013-11-19 15:05:34 -0500

    added IO functions

--- mpptc.c
+++ mpptc.c
@@ -1,64 +1,83 @@
 #include <stdio.h>
 
-#define PM_PIN__DO 9 // digital output
-#define BUCK_PIN__PWM  "P8_13.16"  // pwm
-#define BOOST_PIN__PWM "P9_14.18" // pwm
-#define V_IN_SENSE__ADC 12 // ADC Input
-#define V_BATT_SENSE__ADC 13  // ADC Input
-#define CRNT_SENSE_IN__ADC 14 // ADC Input
-#define CRNT_SENSE_OUT__ADC 15 // ADC Input
+#define PANEL_ENABLE__GPIO 60 // digital output -- P9-12
+#define BUCK_PIN__PWM  "P8_13.16" // pwm output -- P8-13
+#define BOOST_PIN__PWM "P9_14.18" // pwm output -- P9-14
+#define PANEL_VOLTAGE__ADC 0 // ADC Input       -- P9-39
+#define BATT_VOLTAGE__ADC 1  // ADC Input       -- P9-40
+#define CRNT_SENSE_IN__ADC 2 // ADC Input       -- P9-37
+#define CRNT_SENSE_OUT__ADC 3 // ADC Input      -- P9-38
 
-#define PWM_PERIOD 9000 // in ??
-#define VB100 12.65     // in volts
+#define PWM_FREQ 10000 // in Hz
+#define BATT_VOLTAGE_100_PERCENT 12.65     // in volts
 #define VREF 13.5       // in volts
 
+// Constants
 #define TRUE 1
 #define FALSE 0
-
 #define ON 1
 #define OFF 0
 
+// function prototypes
+void panelMode(void);
+void buckMode(double chargerVoltage);
+void boostMode(double chargerVoltage);
+void idleMode(void);
+void setDutyCyclePercentForOutput(int percent, int fd);
+void setOutputForDigitalPin(int aState, int fd);
+double getVoltageforInput(int aPin);
+int confiugrePinAsInput(int aPin);
+int configurePinAsPWM(const char *aPin, int aFreq);
+int configurePinAsOutput(int aPin);
 
-void setDutyCyclePercentForOutput(int percent, const char *pin);
-void setOutputForDigitalPin(int aState, int pin);
+int daemonize(void);
+void sigINT_handler(int signum);
+void sigTERM_handler(int signum);
+void closeConnections(void);
 
-int buckPWM, bootPWM, panelModeState;
+// Global Variables
+int buckDuty, boostDuty, panelModeState, period_ns;
+int buckPin, boostPin, panelEnablePin, panelVoltagePin, battVoltagePin;
 
-int main(void)
-{
     /* 
     Notes:
       Vref=13.5V in charge mode; Vb100=12.65V 100% charged, Vb75=12.45V 75% charged, Vb50=12.24V 50% charge
       d_buck_start and d_boost_start are duty cycle for starte up
     */
 
+int main(void)
+{
+
     double panelVoltage = -1, vBattPanelOff = -1, vBattPanelOn = -1;
 
-    /*start up mode*/
-    //set BUCK_PIN and BOOST_PIN to LOW
-    //read in V_IN_SENSE
-    //read in V_BATT_SENSE
-    //if V_BATT_SENSE is equal or greater to Vb100 stop chargeing go into idle mode
-    //if V_IN_SENSE is equal to Vref, go into panel mode
-    //if panel voltage is higher than Vref, go into buck mode, send d_buck_start = Vref/V_IN_SENSE
-    //if panel voltage is lower than Vref, go into boost mode, send d_boost_start = V_IN_SENSE/Vref
+    buckDuty = 0;
+    boostDuty = 0;
+    period_ns = 0;
+
+    daemonize();
+
+    buckPin = configurePinAsPWM(BUCK_PIN__PWM, PWM_FREQ);
+    boostPin = configurePinAsPWM(BOOST_PIN__PWM), PWM_FREQ;
+    panelEnablePin = configurePinAsOutput(PANEL_ENABLE__GPIO);
+    panelVoltagePin = configurePinAsInput(PANEL_VOLTAGE__ADC);
+    battVoltagePin = configurePinAsInput(BATT_VOLTAGE__ADC);
+
 
     while(TRUE) 
     {
-        /*charge mode*/
-        ////sense panel voltage
 
-        //set BUCK_PIN and BOOST_PIN to LOW
-        setDutyCyclePercentForOutput(0, BUCK_PIN__PWM);
-        setDutyCyclePercentForOutput(0, BOOST_PIN__PWM);
+        // turn off all charging
+        setDutyCyclePercentForOutput(OFF, buckPin);
+        setDutyCyclePercentForOutput(OFF, boostPin);
+        setOutputForDigitalPin(OFF, panelEnablePin);
 
         //read in V_IN_SENSE
-        panelVoltage = getVoltageforInput(V_IN_SENSE__ADC); 
+        panelVoltage = getVoltageforInput(panelVoltagePin); 
         //read in V_BATT_SENSE
-        vBattPanelOff = getVoltageforInput(V_BATT_SENSE__ADC);
+        vBattPanelOff = getVoltageforInput(battVoltagePin);
 
         //if V_BATT_SENSE is equal or greater to Vb100 stop charging go into idle mode
-        if (vBattPanelOff >= VB100)
+        if (vBattPanelOff >= BATT_VOLTAGE_100_PERCENT)
         {
             // fully charged, so shutdown all charging
             idleMode();
@@ -66,13 +85,12 @@ int main(void)
         else
         {
             // restore previous duty cycle because it was correct
-            setDutyCyclePercentForOutput(buckPWM, BUCK_PIN__PWM);
-            setDutyCyclePercentForOutput(boostPWM, BOOST_PIN__PWM);
+            setDutyCyclePercentForOutput(buckDuty, buckPin);
+            setDutyCyclePercentForOutput(boostDuty, boostPin);
 
-            vBattPanelOn = getVoltageforInput(V_BATT_SENSE__ADC);
+            vBattPanelOn = getVoltageforInput(battVoltagePin);
 
-            //if V_IN_SENSE is equal to Vref, go into panel mode
-            if (panelVoltage == vREF)
+            if (panelVoltage == VREF)
             {
                 panelMode();
 
@@ -93,92 +111,269 @@ int main(void)
         sleep(1);
     }
 }
-   ////panel mode
-void panelMode()
+
+void panelMode(void)
 {
-    buckPWM = 0;
-    boostPWM = 0;
+    buckDuty = OFF;
+    boostDuty = OFF;
     panelModeState = ON;
 
-    setDutyCyclePercentForOutput(buckPWM, BUCK_PIN__PWM);
-    setDutyCyclePercentForOutput(boostPWM, BOOST_PIN__PWM);
-    setOutputForDigitalPin(panelModeState, PM_PIN__DO);
+    setDutyCyclePercentForOutput(buckDuty, buckPin);
+    setDutyCyclePercentForOutput(boostDuty, boostPin);
+    setOutputForDigitalPin(panelModeState, panelEnablePin);
 
 }
 
-   ////buck mode
 void buckMode(double chargerVoltage)
- {
-
+{
     panelModeState = OFF;
-    setOutputForDigitalPin(panelModeState, PM_PIN__DO);
+    setOutputForDigitalPin(panelModeState, panelEnablePin);
 
     // turn boost off
-    boostPWM = 100;
-    setDutyCyclePercentForOutput(boostPWM, BOOST_PIN__PWM);
+    boostDuty = OFF;
+    setDutyCyclePercentForOutput(boostDuty, boostPin);
 
    //read in V_BATT_SENSE
    //if V_BATT_SENSE greater than Vref
     if (chargerVoltage > VREF)
     {
         // increase buck dutycycle  by 1%
-        buckPWM++;
-        setDutyCyclePercentForOutput(buckPWM, BUCK_PIN__PWM);
+        buckDuty++;
+        setDutyCyclePercentForOutput(buckDuty, buckPin);
     }
 }
 
-   ////boost mode
 void boostMode(double chargerVoltage)
 {
 
     panelModeState = OFF;
-    setOutputForDigitalPin(panelModeState, PM_PIN__DO);
+    setOutputForDigitalPin(panelModeState, panelEnablePin);
 
      // turn boost off
-    buckPWM = 100;
-    setDutyCyclePercentForOutput(buckPWM, BUCK_PIN__PWM);
+    buckDuty = OFF;
+    setDutyCyclePercentForOutput(buckDuty, buckPin);
 
     if (chargerVoltage < VREF)
     {
         // increase buck dutycycle  by 1%
-        boostPWM++;
-        setDutyCyclePercentForOutput(boostPWM, BOOST_PIN__PWM);
+        boostDuty++;
+        setDutyCyclePercentForOutput(boostDuty, boostPin);
     }
 }
 
-void idleMode() 
+void idleMode(void) 
 {
-    buckPWM = 0;
-    boostPWM = 0;
+    buckDuty = OFF;
+    boostDuty = OFF;
     panelModeState = OFF;
 
-    setDutyCyclePercentForOutput(buckPWM, BUCK_PIN__PWM);
-    setDutyCyclePercentForOutput(boostPWM, BOOST_PIN__PWM);
-    setOutputForDigitalPin(panelModeState, PM_PIN__DO);
+    setDutyCyclePercentForOutput(buckDuty, buckPin);
+    setDutyCyclePercentForOutput(boostDuty, boostPin);
+    setOutputForDigitalPin(panelModeState, panelEnablePin);
 }
 
-void setDutyCyclePercentForOutput(int percent, const char *pin)
-{    
-    char command[200];
-
-    int dutyC = (percent / 100.0) * PERIOD;
-    sprintf(command, "echo %d >  /sys/devices/ocp.3/pwm_test_%s/duty", dutyC, pin);
-    printf("DEBUG :: %s\n", command);
-    system(command);
-}
-
-void setOutputForDigitalPin(int aState, int pin)
+void setDutyCyclePercentForOutput(int percent, int fd)
 {
-    char command[200];
+   char command[5];
 
-    sprintf(command, "echo %d > /sys/class/gpio/gpio%d/value", aState, pin);
-    printf("DEBUG :: %s\n", command);
-    system(command);
+    int newDuty = (percent / 100.0) * PERIOD;
+    if (newDuty > 100 || newDuty < 0)
+    {
+        syslog(LOG_INFO, "ERROR: Invalid duty cycle: %d", newDuty);
+        return;
+    }
+    sprintf(command, "%d", newDuty);
+    write(fd, command, strlen(command));
+
 }
 
-double getVoltageforInput(int aPin) 
+void setOutputForDigitalPin(int aState, int fd)
 {
-    cat /sys/bus/iio/devices/iio:device0/in_voltage0_raw
+    if (aState == 1)
+    {
+        write(fd, "1", 1);
+    } else
+    {
+        write(fd, "0", 1);
+    }
+}
+
+double getVoltageforInput(int aPin)
+{
+    int value;
+
+    read(aPin, &value, 1);
+    lseek(aPin, 0, SEEK_SET);
+
+    return value*8.7891e-4;
+}
+
+int confiugrePinAsInput(int aPin)
+{
+    char buf[50];
+
+    sprintf(buf, "/sys/bus/iio/devices/iio:device0/in_voltage%d_raw", aPin);
+
+    fd = open(buf, O_RONLY);
+    if (fd < 1)
+    {
+        syslog(LOG_INFO, "ERROR: Unable to enable ADC pin: %d", aPin);
+        return -1;
+
+    }
+    return fd;
+}
+
+int configurePinAsPWM(const char *aPin, int aFreq)
+{
+    int fd;
+    char buf[100];
+
+    // enable PWM pin
+    fd = open("/sys/devices/bone_capemgr.9/slots", O_WRONLY);
+    if(fd < 1) {
+        syslog(LOG_INFO, "Unable to enable PWM Pin: %s", aPin);
+        return -1;
+    }
+    sprintf(buf, "bone_pwm_%s", aPin); 
+    write(fd, buf, strlen(buf));
+    close(fd);
+
+    // configure Period
+    sprintf(buf, "/sys/devices/ocp.3/pwm_test_%s/period", aPin);
+    fd = open(buf, O_WRONLY);
+    if (fd < 1)
+    {
+        syslog(LOG_INFO, "Unable to initialize PWM period: %s", aPin);
+        return -1;
+    }
+    period_ns = (unsigned long)(1e9 / freq);
+    sprintf(buf, "%lu", period_ns);
+    write(fd, buf, strlen(buf));
+    close(fd);
+
+    // configure duty cycle
+    sprintf(buf, "/sys/devices/ocp.3/pwm_test_%s/duty", aPin);
+    fd = open(buf, O_WRONLY);
+    if (fd < 1)
+    {
+        syslog(LOG_INFO, "Unable to initialize PWM duty: %s", aPin);
+        return -1;
+    }
+    
+    setDutyCyclePercentForOutput(0, fd);
+    return fd;
+}
+
+int configurePinAsOutput(int aPin)
+{
+    int fd;
+    char buf[100];
+
+    // turn pin into IO
+    fd = open("/sys/class/gpio/export", O_WRONLY);
+    if(fd < 1) {
+        syslog(LOG_INFO, "Unable to export IO Pin: %d", aPin);
+        return -1;
+    }
+    sprintf(buf, "%d", aPin); 
+    write(fd, buf, strlen(buf));
+    close(fd);
+
+
+    // turn pin into Output
+    sprintf(buf, "/sys/class/gpio/gpio%d/direction", aPin);
+    fd = open(buf, O_WRONLY);
+    if(fd < 1) {
+        syslog(LOG_INFO, "Unable to initialize IO Pin direction: %d", aPin);
+        return -1;
+    }
+    write(fd, "out", 3); 
+    close(fd);
+
+
+    // set output low
+    sprintf(buf, "/sys/class/gpio/gpio%d/value", gpio);
+    fd = open(buf, O_WRONLY);
+    if(fd < 1) {
+        syslog(LOG_INFO, "Unable to Initialize IO Pin Output to LOW: %d", aPin);
+        return -1;
+    }
+
+    write(fd, "0", 1); 
+    return fd;
+}
+
+int daemonize(void) 
+{
+    /* Our process ID and Session ID */
+    pid_t pid, sid;
+    
+    /* Fork off the parent process */
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    /* If we got a good PID, then
+       we can exit the parent process. */
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    /* Change the file mode mask */
+    umask(0);
+            
+    /* Open any logs here */ 
+    openlog("MPPTC", LOG_PID|LOG_CONS, LOG_USER);
+    syslog(LOG_INFO, "Daemon Started.\n");
+            
+    /* Create a new SID for the child process */
+    sid = setsid();
+    if (sid < 0) {
+        /* Log the failure */
+        syslog(LOG_INFO, "ERROR :: Unable to create new SID. Exiting.");
+        exit(EXIT_FAILURE);
+    }
+    syslog(LOG_INFO, "finished forking");
+    
+    /* Change the current working directory */
+    if ((chdir("/")) < 0) {
+        /* Log the failure */
+        syslog(LOG_INFO, "ERROR :: Unable to change working directory. Exiting");
+        exit(EXIT_FAILURE);
+    }
+    
+    /* Close out the standard file descriptors */
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+    
+    /* Daemon-specific initialization goes here */
+    
+    /* Register Signal Handlers*/
+    signal(SIGTERM, sigTERM_handler);
+    signal(SIGINT, sigINT_handler);
+    return 0;
+ }
+ 
+void sigINT_handler(int signum)
+{
+    syslog (LOG_INFO, "Caught SIGINT. Exiting...");
+    closeConnections();
+    exit(0);
+
+}
+
+void sigTERM_handler(int signum)
+{
+    syslog(LOG_INFO, "Caught SIGTERM. Exiting...");
+    closeConnections();
+    exit(0);
+}
+
+void closeConnections(void)
+{
+    
 }
 
 
